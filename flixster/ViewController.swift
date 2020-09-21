611cedba15665683adcb6ae709906e100a7a8a1d
Yasiris Ortiz <yasirisortiz24@gmail.com>
2020-09-21 19:40:27 -0400

    initial commit

---
+++ flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flixster
+//
+//  Created by Yasiris Ortiz on 19.09.20.
+//  Copyright © 2020 Yasiris Ortiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
